a74c5a47b0de5f23e388f052fc2d3e49f99a370a
Adilson da Silva Guilherme <74576838+adilsoftware@users.noreply.github.com>
2021-08-30 13:38:10 -0300

    Update excluir5.php

--- adilsoftware.atwebpages.com/excluir5.php
+++ adilsoftware.atwebpages.com/excluir5.php
@@ -4,7 +4,7 @@ $usuario = $_GET['nome'];
 $fone = $_GET['fone'];
 $idfornecedor = $_GET['idfornecedor'];
 
-$con=mysqli_connect('fdb30.awardspace.net', '3639130_adilsoftware', 'ad81guilherme', '3639130_adilsoftware');
+$con=mysqli_connect('fdb30.awardspace.net', '', '', '');
         
         
         
